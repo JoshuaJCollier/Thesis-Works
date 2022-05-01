17f79a83ce60f75dc0412bb42e62db53edcfe270
Josh <josh@creativeworks.com.au>
2022-05-01 14:07:38 +0800

    Update testing.py
    split into functions, loop working, starting gradient

--- testing.py
+++ testing.py
@@ -1,5 +1,3 @@
-print('Starting...')
-
 from redpitaya.overlay.mercury import mercury as FPGA
 import IPython.display as ipd
 import time
@@ -8,60 +6,163 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 import random
+import gc
 
-fpga = FPGA()
+def initialise(sampleRate):
+    print('Beginning Initialisation...')
+    fpga = FPGA()
 
-out1 = fpga.gen(0)
-out2 = fpga.gen(1)
-in1 = fpga.osc(0, 1.0) #Instance of oscilliscope object for the input
-in2 = fpga.osc(1, 1.0) 
-#First arguement is the channel index, either 0 or 1 (this is In1=0, In2=1)
-#Second argument is the voltage range, either 1.0 (default) or 20.0
+    # Initialisation of inputs and outputs (oscilliscopes and generators)
+    out1 = fpga.gen(0) # Output to X-Axis K-Cube
+    out2 = fpga.gen(1) # Output to Y-Axis K-Cube
+    in1 = fpga.osc(0, 1.0) # Instance of oscilliscope object for the input
+    # in2 = fpga.osc(1, 1.0) # Unused input
 
-print(type(out1))
-# buffer waveform and sample timing
-out1.waveform      = out1.sin()
-out1.frequency = 0
+    # Initialise output as sinusoid with no frequency or amplitude, as the output will be
+    # controlled by manipulating the offset as the program runs, this initialisation is
+    # the same for both outputs
+    out1.waveform = out1.sin()
+    out1.frequency = 0
+    out1.amplitude = 0.0
+    out1.offset = 0
+    out1.enable = True
 
-# set output amplitude, offset and enable it
-out1.amplitude     = 0.0
+    out2.waveform = out2.sin()
+    out2.frequency = 0
+    out2.amplitude = 0.0
+    out2.offset = 0
+    out2.enable = True
 
-# buffer waveform and sample timing
-out2.waveform      = out1.sin()
-out2.frequency = 0
+    in1.decimation = int(125000000/sampleRate) # 125Msps / 125Ksps = 1000
+    in1.enable = True
+    # dir(in1)
 
-# set output amplitude, offset and enable it
-out2.amplitude     = 0.0
+    # out2.sync_src = fpga.sync_src['gen0']
+    print('System Initialised :)')
+    #print(fpga.overlay)
 
-in1.decimation = int(65536/2) # 125Msps / 65536 = 125Ksps
+    return in1, out1, out2
 
-out2.sync_src = fpga.sync_src['gen0']
+def run(sampleRate, runTime, size, climb):
+    """Runs system for set period of time
 
-out1.offset = 0
-out2.offset = 0
-out1.enable        = True
-out2.enable        = True
-
-finalArray = np.array([0])
-start = time.perf_counter()
-out1.start()
-out2.start()
-out1.trigger()
-output = []
-
-for i in range(100):
-    out1.offset        = 0.02*i-1
-    #out2.offset        = 0.02*i-1
-
+    Parameters:
+    sampleRate (int): Number of samples per second (to pass onto initialise)
+    runTime (int): Number of seconds to run for
+    size (int): Size of output array
+    climb (bool): Boolean to determine whether or not to use the climbing algorithm
     
-    output.append(np.average(in1.data(60)))
+    Returns:
+    output (np.array): Output of last (size) values read from input
+    """
     
-    time.sleep(0.004)
-    # NOTE: Without the append, the loop takes 0.37s for 10,000 iterations, which are outputted correctly
-    #       this means that the maximum frequency of the system without maths is ~27,000Hz
+    in1, out1, out2 = initialise(sampleRate)
+    in1.start()
+    out1.start()
+    out2.start()
+    out1.trigger()
+    out2.trigger()
+
+    output = np.zeros(size)
+    inputx = np.zeros(size)
+    inputy = np.zeros(size)
     
-#print(help(in1.data(60)))
-end = time.perf_counter() - start
-print("Time taken: {:.2f}s, done".format(end))
-out1.enable = False
-out2.enable = False
+    x_val = 0
+    y_val = 0
+    out1.offset = x_val
+    out2.offset = y_val
+    time.sleep(0.5)
+    
+    count = 1
+    correct = 1
+    
+    perIteration = 0.005
+    
+    print('Running')
+    start = time.perf_counter()
+    instanceStart = time.perf_counter()
+    while((time.perf_counter() - start) <= runTime):
+        #x_val = math.sin(time.perf_counter()*math.pi)
+        #y_val += 0.002
+        direction = 0
+        valueX = 0
+        valueY = 0
+        if climb:
+            direction = random.random()*2*math.pi
+            valueX = math.cos(direction)*0.05
+            valueY = math.sin(direction)*0.05
+        
+        x_val += valueX
+        y_val += valueY
+        
+        if (x_val > 1):
+            x_val = 1
+        if (x_val < -1):
+            x_val = -1
+        if (y_val > 1):
+            y_val = 1
+        if (y_val < -1):
+            y_val = -1
+        
+        out1.offset = x_val
+        out2.offset = y_val
+        
+        while ((time.perf_counter() - instanceStart) < perIteration): pass
+        # print(time.perf_counter() - instanceStart)
+        # NOTE: Without the append, the loop takes 0.37s for 10,000 iterations, which are outputted correctly
+        #       this means that the maximum frequency of the system without maths is ~27,000Hz
+        instanceStart = time.perf_counter()
+
+        output[count%size] = np.average(in1.data(int(sampleRate/maxRate*perIteration*100000000)))
+        inputx[count%size] = x_val
+        inputy[count%size] = y_val
+
+        if (output[count%size] < output[(count-1)%size]):
+            x_val -= 2*valueX
+            y_val -= 2*valueY
+            correct += 1
+        count += 1
+
+    end = time.perf_counter() - start
+
+    print("Time taken: {:.2f}s, done".format(end))
+    print("Count: {}".format(count))
+    print("Corrections: {:2f}%".format((correct*100)/count))
+    print("System Frequency: {:.2f}Hz".format(count/(end)))
+    
+    in1.stop()    
+    out1.stop()
+    out2.stop()
+    
+    return output, inputx, inputy
+
+frequency = 200
+seconds = 5
+maxRate = 125000000
+sampleRate = 1250000
+
+output, inputx, inputy = run(sampleRate, seconds, seconds*frequency, True)
+output2, inputx2, inputy2 = run(sampleRate, seconds, seconds*frequency, False)
+
+plt.figure(0)
+plt.plot(inputx)
+plt.plot(inputy)
+plt.plot(output)
+plt.ylim(-1, 1)
+plt.title('Climb')
+plt.legend(['X','Y','Out'])
+#plt.xlim(50,60)
+
+plt.figure(1)
+plt.plot(inputx2)
+plt.plot(inputy2)
+plt.plot(output2)
+plt.ylim(-1, 1)
+plt.title('No Climb')
+plt.legend(['X','Y','Out'])
+#plt.xlim(50,60)
+
+print('Climb Avg: {:2f}V'.format(np.average(output)))
+print('No Climb Avg: {:2f}V'.format(np.average(output2)))
+
+plt.show()

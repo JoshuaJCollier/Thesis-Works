9f25940be7643082cadad94ced00aec408825252
Josh <josh@creativeworks.com.au>
2022-05-17 11:27:27 +0800

    added 3d plot

--- plotData.py
+++ plotData.py
@@ -1,7 +1,10 @@
-from redpitaya.overlay.mercury import mercury as FPGA
-#import numpy as np
-import matplotlib.pyplot as plt
 from gradientAscent import *
+from redpitaya.overlay.mercury import mercury as FPGA
+from mpl_toolkits.mplot3d import Axes3D
+from scipy.stats import multivariate_normal
+import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 seconds = 5
 sampleRate = 125000000
@@ -75,3 +78,40 @@ print('Climb Tot Avg: {:2f}V'.format(np.average(finalAvg)))
     
 for i in range(1):
     print('Non Climb Avg: {:2f}V'.format(np.average(outNoClimbArray[i])))
+
+def plot3D():
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d',alpha=0.1)
+
+    x, y = np.mgrid[-1.0:1.0:30j, -1.0:1.0:30j]
+    xy = np.column_stack([x.flat, y.flat])
+
+    mu = np.array([0.0, 0.0])
+    sigma = np.array([0.2, 0.2])
+    covariance = np.diag(sigma**2)
+
+    z = multivariate_normal.pdf(xy, mean=mu, cov=covariance)
+    z = z.reshape(x.shape)*1/np.max(z)*sum(output[int(len(output)/2):])/(len(output)/2)
+    x = x + sum(inputx[int(len(inputx)/2):])/(len(inputx)/2)
+    y = y + sum(inputy[int(len(inputy)/2):])/(len(inputy)/2)
+    ax.plot_wireframe(x,y,z,color='grey')
+    length = len(inputx)
+
+    # plotting directly
+    ax.plot(inputx[0:length-2], inputy[0:length-2], output[0:length-2], color='red')
+
+    # plotting via scatter
+    # ax.scatter(inputx, inputy, output, s=1)
+
+    # plotting via quivers
+    '''
+    for i in range(length-1):
+        if (output[i] < sum(output)/length):
+            soa = np.array([[inputx[i], inputy[i], output[i], inputx[i+1]-inputx[i], inputy[i+1]-inputy[i], output[i+1]-output[i]]])
+            dist = math.sqrt((inputx[i+1]-inputx[i])**2 + (inputy[i+1]-inputy[i])**2 + (output[i+1]-output[i])**2)
+            
+            X, Y, Z, U, V, W = zip(*soa)
+            ax.quiver(X, Y, Z, U, V, W,color='r',length=dist)
+    '''
+
+    plt.show()

3234fc5d5e7918831052468fc29e37acf3cc5c5f
Josh <josh@creativeworks.com.au>
2022-06-16 16:38:33 +0800

    Update gradientAscentC.h

--- gradientAscentC.h
+++ gradientAscentC.h
@@ -2,11 +2,12 @@
 typedef struct run_in {
     int sampleRate;
     int size;
+    int runNum;
     bool climb;
     float stepSize;
     double runTime;
     double frequency;
 } run_in;
 
-int save(int size, float[] inx, float[] iny, float[] out);
-int[] run(struct run_in in);
+int save(int size, int indexNum, float* inx, float* iny, float* out);
+int run(struct run_in in);

21d8d75457245603aade4e42b44576030dffa20f
Josh <josh@creativeworks.com.au>
2022-06-16 17:59:00 +0800

    Update gradientAscentC.h

--- gradientAscentC.h
+++ gradientAscentC.h
@@ -2,12 +2,18 @@
 typedef struct run_in {
     int sampleRate;
     int size;
-    int runNum;
-    bool climb;
+    bool climb; // Use the climbing algorithm
+    bool climbGrad; // Use the gradient aspect in climbing algorithm
+    bool climbSmart; // Use past output values for maths
+    int climbMulti; // Use for multiple moves per iteration
     float stepSize;
     double runTime;
     double frequency;
+    uint32_t buffSize;
 } run_in;
 
-int save(int size, int indexNum, float* inx, float* iny, float* out);
+int initialise();
+int save(struct run_in in, float* inx, float* iny, float* out);
+void generateMove(float stepSize, float* x_val, float* y_val, float* x_change, float* y_change);
+void correctOut(float* currOut, float lastOut, bool climbGrad, bool climbSmart, float stepSize, float* x_val, float* y_val, float x_change, float y_change);
 int run(struct run_in in);
